20e528f16af8232de58c314858a282373567bb1c
Maximilien Icart <icart.maximilien@gmail.com>
2018-11-30 16:47:55 +0100

    WIP

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,26 @@ const router = express.Router();
 const Registration = mongoose.model('Registration');
 
 router.get('/', (req, res) => {
-    res.render('form', { title: 'Registration form' });
+    res.render('form', { title: 'BDD MONGODB' });
 });
 
 
-router.post('/',
-    [
-        body('name')
-        .isLength({ min: 1 })
-        .withMessage('Please enter a name'),
-        body('email')
-        .isLength({ min: 1 })
-        .withMessage('Please enter an email'),
-    ],
-    (req, res) => {
-        const errors = validationResult(req);
-    
-        if (errors.isEmpty()) {
-            const registration = new Registration(req.body);
-            registration.save()
-                .then(() => { res.send('Thank you for your registration!'); })
-                .catch(() => { res.send('Sorry! Something went wrong.'); });
-            } else {
-            res.render('form', {
-                title: 'Registration form',
-                errors: errors.array(),
-                data: req.body,
-            });
-            }
+router.post('/add-user', (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+        const registration = new Registration(req.body);
+        registration.save()
+            .then(() => { res.send('Thank you for your registration!'); })
+            .catch(() => { res.send('Sorry! Something went wrong.'); });
+        } else {
+        res.render('form', {
+            title: 'Registration form',
+            errors: errors.array(),
+            data: req.body,
+        });
     }
-);
+});
 
 
 module.exports = router;
